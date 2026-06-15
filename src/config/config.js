b43fccdaedadaeef6c1a8f0a078b2dfbe87e18b2
dlUNA78 <dajelu2003@gmail.com>
2026-06-15 10:37:07 -0600

    Cambio de dependencias a pnpm

--- src/config/config.js
+++ src/config/config.js
@@ -2,19 +2,19 @@ import 'dotenv/config';
 
 export default {
   development: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || 'tu_contraseña',
-    database: process.env.DB_NAME || 'agrosystem_db',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'postgres'
   },
   test: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || 'tu_contraseña',
-    database: process.env.DB_NAME || 'agrosystem_db_test',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'postgres'
   },
   production: {
