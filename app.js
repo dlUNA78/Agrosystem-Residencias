2efcfa42fe68a3deff541bbd6fb22bf4ffd62fdf
123456789493 <marquezamerica51@gmail.com>
2026-06-17 19:23:52 -0600

    last test

--- app.js
+++ app.js
@@ -52,8 +52,7 @@ try {
     });
 } catch (error) {
     console.error('❌ Error al conectar con la base de datos PostgreSQL:', error);
-    process.exit(1); // Detiene el proceso si no hay base de datos como se solicita
-    //Prueba de que si funcionaron los cambios
+    process.exit(1); // Ultima prueba
 }
 
 export default app;
